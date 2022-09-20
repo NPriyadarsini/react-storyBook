7cf3aad1383a50b3d1059b388fef433ab366fb75
PriyadarsiniNandakumar <pdn@laufire.in>
2022-09-20 19:34:27 +0530

    Input Refactoring

--- src/stories/Input.stories.jsx
+++ src/stories/Input.stories.jsx
@@ -8,30 +8,8 @@ export default {
 
 const Template = (args) => <Input { ...args }/>;
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-	primary: true,
-	label: 'Input',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-	label: 'Input',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-	size: 'large',
-	label: 'Input',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-	size: 'small',
-	label: 'Input',
-};
 export const TextBox = Template.bind({});
 TextBox.args = {
+	size: 'small',
 	label: 'Input',
 };
